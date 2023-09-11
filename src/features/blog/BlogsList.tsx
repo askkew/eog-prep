8c8e7fc6938cb6492fd124c38d1dee50ac0f36ab
Lucas Overbey <lucasaoverbey@gmail.com>
2023-09-11 14:48:09 -0500

    styling

--- src/features/blog/BlogsList.tsx
+++ src/features/blog/BlogsList.tsx
@@ -3,8 +3,28 @@ import { useSelector } from 'react-redux'
 import { selectAllBlogs } from './blogSlice'
 import { Button, Card, FormControl, TextField } from '@mui/material'
 import styled from '@emotion/styled'
+import SimpleBar from "simplebar-react";
+import 'simplebar-react/dist/simplebar.min.css';
+import { createTheme, ThemeProvider, Theme, useTheme } from '@mui/material/styles';
 
 // =============== Styles =================
+const NewBlogContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+  padding: '1em',
+  border: '2px solid gainsboro',
+  borderRadius: '12px',
+  gap: '0.5em',
+  width: 'clamp(150px, 80%, 350px)',
+  height: '250px',
+})
+
+const CustomTextField = styled(TextField)({
+  width: '100%',
+})
+
 const EnterBlogContainer = styled('div')({
   display: 'flex',
   flexDirection: 'column',
@@ -34,11 +54,56 @@ const BlogGrid = styled('div')({
   alignContent: 'flex-start',
   gap: '2em',
   width: '100vw',
+  marginTop: '1em',
 })
+
+const customTheme = (outerTheme: any) =>
+  createTheme({
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            '--TextField-brandBorderColor': 'rgba(255, 255, 255, 0.85)', // Change border color to white
+            '--TextField-brandBorderHoverColor': '#B2BAC2', // Change hover border color (if needed)
+            '--TextField-brandBorderFocusedColor': '#6F7E8C', // Change focused border color (if needed)
+            '& label.Mui-focused': {
+              color: 'var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            '& $notchedOutline': {
+              borderColor: 'var(--TextField-brandBorderColor)', // Change border color to white
+            },
+            '&:hover $notchedOutline': {
+              borderColor: 'var(--TextField-brandBorderHoverColor)', // Change hover border color (if needed)
+            },
+            '&.Mui-focused $notchedOutline': {
+              borderColor: 'var(--TextField-brandBorderFocusedColor)', // Change focused border color (if needed)
+            },
+          },
+        },
+      },
+      MuiInputBase: {
+        styleOverrides: {
+          input: {
+            color: 'white', // Change text color to white
+          },
+        },
+      },
+    },
+  });
 // ========================================
 
 
 const BlogsList = () => {
+  const outerTheme = useTheme();
 
   const blogs = useSelector(selectAllBlogs)
 
@@ -53,15 +118,18 @@ const BlogsList = () => {
 
   return (
     <section id="blogsection">
-      <h2>Insert a new blog!</h2>
-      <FormControl>
-        <TextField></TextField>
-        <Button variant="contained"> Submit </Button>
-      </FormControl>
-      <h2>Blogs</h2>
-      <BlogGrid>
-        {renderedBlogs}
-      </BlogGrid>
+      <SimpleBar style={{ maxHeight: '100vh' }}>
+        <BlogGrid>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <NewBlogContainer>
+              <CustomTextField variant="outlined" placeholder="Enter blog title" id="mui-theme-provider-outlined-input" />
+              <CustomTextField variant="outlined" placeholder="Enter blog content" id="mui-theme-provider-outlined-input" multiline rows={5}/>
+              <Button variant="contained"> Submit </Button>
+            </NewBlogContainer>
+          </ThemeProvider>
+          {renderedBlogs}
+        </BlogGrid>
+      </SimpleBar>
     </section>
   )
 }
